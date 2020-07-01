c26ba89aa3423b4b2fd52105521aecdee0c30a31
Razvan <razvan.dajboc@bertelsmann.de>
2020-07-01 15:00:20 +0300

    Initialize Spring backend-project

---
+++ coffee-service/src/main/java/com/arvato/coffeeservice/CoffeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arvato.coffeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeServiceApplication.class, args);
+    }
+
+}
---
+++ coffee-service/src/test/java/com/arvato/coffeeservice/CoffeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arvato.coffeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
