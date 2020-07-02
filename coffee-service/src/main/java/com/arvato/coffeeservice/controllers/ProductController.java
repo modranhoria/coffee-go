a2ad4b80b26ef13b74392365add25638b8691341
roga027 <sindija.roga@bertelsmann.de>
2020-07-02 12:06:21 +0300

    finished product service (ProductController)

--- coffee-service/src/main/java/com/arvato/coffeeservice/controllers/ProductController.java
+++ coffee-service/src/main/java/com/arvato/coffeeservice/controllers/ProductController.java
@@ -3,11 +3,10 @@ package com.arvato.coffeeservice.controllers;
 import com.arvato.coffeeservice.models.Product;
 import com.arvato.coffeeservice.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("products")
@@ -19,4 +18,29 @@ public class ProductController {
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
+
+    @PostMapping("/product")
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.saveAndFlush(product);
+    }
+
+    @PutMapping("/product/{id}")
+    public Product updateProductById(@RequestBody Product product, @PathVariable("id") Long productId) {
+        Optional<Product> productById = productRepository.findById(productId);
+
+        if (productById.isPresent()) {
+            productById.get().setName(product.getName());
+            productById.get().setDescription(product.getDescription());
+            productById.get().setQuantity(product.getQuantity());
+            productById.get().setPrice(product.getPrice());
+        }
+
+        return productRepository.saveAndFlush(productById.get());
+    }
+
+    @DeleteMapping("/product/{id}")
+    public void deleteProductById(@PathVariable("id") Long productId) {
+        productRepository.deleteById(productId);
+    }
+
 }
