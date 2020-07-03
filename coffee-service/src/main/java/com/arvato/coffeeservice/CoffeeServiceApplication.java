da1049b08d8ca7d14bd622653b04c1ef78773ce9
Razvan <razvan.dajboc@bertelsmann.de>
2020-07-03 14:08:36 +0300

    Async Product import

--- coffee-service/src/main/java/com/arvato/coffeeservice/CoffeeServiceApplication.java
+++ coffee-service/src/main/java/com/arvato/coffeeservice/CoffeeServiceApplication.java
@@ -2,12 +2,26 @@ package com.arvato.coffeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@EnableAsync
 public class CoffeeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoffeeServiceApplication.class, args);
     }
 
+    @Bean("threadPoolTaskExecutor")
+    public TaskExecutor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(1000);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setThreadNamePrefix("Async-");
+        return executor;
+    }
 }
